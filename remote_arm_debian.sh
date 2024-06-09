faa61e4d6f620d455258ccee93311e81634fa1af
SoundOfTwitter <140434271+SoundOfTwitter@users.noreply.github.com>
2024-06-09 23:42:28 +0800

    Create remote_arm_debian.sh

---
+++ remote_arm_debian.sh
@@ -0,0 +1,48 @@
+cd /home/admin
+apt install wget -y
+apt install tar -y
+apt install vim -y
+apt update -y
+apt upgrade -y
+apt install language-pack-zh-hans -y
+update-locale LANG=zh_CN.UTF-8
+
+# 可能无需运行
+dpkg --configure -a
+# 可能无需运行
+apt --fix-broken install
+
+apt install task-xfce-desktop -y
+apt install xorgxrdp -y
+apt install xrdp -y
+adduser xrdp ssl-cert
+systemctl restart xrdp
+wget https://github.com/fatedier/frp/releases/download/v0.34.2/frp_0.34.2_linux_arm64.tar.gz
+tar -zxvf frp_0.34.2_linux_arm64.tar.gz
+cd /home/admin/frp_0.34.2_linux_arm64
+sed -i '4i\token = 51247864' frpc.ini
+sed -i '6c\[13389]' frpc.ini
+sed -i '9c\local_port = 3389' frpc.ini
+sed -i '10c\remote_port = 13389' frpc.ini
+echo 'token = 51247864' >> /home/admin/frp_0.34.2_linux_arm64/frps.ini
+mkdir /etc/frp
+cp /home/admin/frp_0.34.2_linux_arm64/frpc /etc/frp
+cp /home/admin/frp_0.34.2_linux_arm64/frpc.ini /etc/frp
+cp /home/admin/frp_0.34.2_linux_arm64/frpc_full.ini /etc/frp
+cp /home/admin/frp_0.34.2_linux_arm64/frps /etc/frp
+cp /home/admin/frp_0.34.2_linux_arm64/frps.ini /etc/frp
+cp /home/admin/frp_0.34.2_linux_arm64/frps_full.ini /etc/frp
+cp /home/admin/frp_0.34.2_linux_arm64/systemd/frpc.service /etc/systemd/system
+cp /home/admin/frp_0.34.2_linux_arm64/systemd/frps.service /etc/systemd/system
+chmod 754 /etc/systemd/system/frpc.service
+chmod 754 /etc/systemd/system/frps.service
+systemctl enable /etc/systemd/system/frpc.service
+systemctl enable /etc/systemd/system/frps.service
+cp /home/admin/frp_0.34.2_linux_arm64/frpc /usr/bin
+chmod +x /usr/bin/frpc
+systemctl start frpc
+ps -ef|grep frpc
+cp /home/admin/frp_0.34.2_linux_arm64/frps /usr/bin
+chmod +x /usr/bin/frps
+systemctl start frps
+ps -ef|grep frps
